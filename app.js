40e0756ce9dbfd6da7ef6160c30c838dc0d0cb21
Net <vnet@FVFZMYHCJ1WK.tld>
2022-04-20 08:32:41 -0500

    rbkj

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ function getCard(){
 var humScore = []
 
 function score(){
-<<<<<<< HEAD
     
 }
 
@@ -25,15 +24,3 @@ function score(){
 function reloadCard(){
     document. location. reload()
 }
-=======
-    var num = getCard()
-    var x = document.getElementById("humTotal");
-
-    for(var i = 1; i <= num; i++){
-        humScore.push("w")
-    }
-
-    x.innerHTML = `Human total is ${humScore.length}.`
-}
-
->>>>>>> 965b8b2049283de9335a7f59f64572fe20d33a34
