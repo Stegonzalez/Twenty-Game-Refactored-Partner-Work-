76be979685392c7a0f2893f32f95e8987b87d93f
Gonzalez Sanchez <stegonzalezsanchez@FVFZMYE6J1WK.tld>
2022-04-22 09:04:54 -0500

    fixed computer function

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ function getCard(){
     var suitArray = ["C", "D", "H", "S"]
     var randomSuit = Math.floor(Math.random() * 4)
 
-    document.getElementById("card").insertAdjacentHTML("beforeend", `<image src = images/${r}${suitArray[randomSuit]}.jpg>`)
+    document.getElementById("userCard").insertAdjacentHTML("beforeend", `<image src = images/${r}${suitArray[randomSuit]}.jpg>`)
     return r
 }
 
@@ -24,11 +24,32 @@ function score(){
     }
 
     x.innerHTML = `Human total is ${humScore.length}.`
+
+    return humScore.length
 }
 
-function noMore(){
-    var num = getCard()
+var compScore = []
+
+function getCompCard(){
+    do{
+        var c = Math.floor(Math.random() * 10) + 1
+        var suitArray = ["C", "D", "H", "S"]
+        var randomSuit = Math.floor(Math.random() * 4)
+
+        document.getElementById("compCard").insertAdjacentHTML("beforeend", `<image src = images/${c}${suitArray[randomSuit]}.jpg>`)
+
+        for(var i = 1; i <= c; i++){
+            compScore.push("w")
+        }
+    
+    }while(compScore.length < 16)
+
+    document.getElementById("compTotal").innerHTML = `The Computer total is ${compScore.length}.`
+
+    return compScore.length
 }
+
+
 
 function reloadCard(){
     document.location.reload()
